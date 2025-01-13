235570ef9bda8e0936112b4a4ec0354141eeeb3b
isaacfaa <takumiosu@gmail.com>
2025-01-13 22:09:08 +0800

    Completed the container orchestration of app

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 // Serve static files from the public directory
 app.use(express.static("./public"));
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const { addAccount, login } = require('./utils/AccountUtil')
